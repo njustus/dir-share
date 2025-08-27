da7812c286780708e5f617362e5a5b10cdd18367
N. Justus <n-justus@outlook.com>
2025-08-27 18:56:47 +0200

    rm unused endpoints

--- backend/src/main/scala/com/github/njustus/localshare/backend/Endpoints.scala
+++ backend/src/main/scala/com/github/njustus/localshare/backend/Endpoints.scala
@@ -3,7 +3,7 @@ package com.github.njustus.localshare.backend
 import cats.effect.IO
 import com.github.njustus.localshare.shared.FilesEndpoints.{FileEntry, MultipartUpload}
 import com.github.njustus.localshare.*
-import com.github.njustus.localshare.shared.{FilesEndpoints, Library, LibraryEndpoints, UserEndpoints}
+import com.github.njustus.localshare.shared.FilesEndpoints
 import sttp.tapir.*
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
@@ -38,13 +38,8 @@ trait FilesServerEndpoints extends FilesEndpoints {
   val endpoints: List[ServerEndpoint[Any, IO]] = List(listServerEndpoint, uploadServerEndpoint)
 }
 
-object Endpoints extends UserEndpoints with LibraryEndpoints with FilesServerEndpoints {
-  val helloServerEndpoint: ServerEndpoint[Any, IO] =
-    helloEndpoint.serverLogicSuccess(user => IO.pure(s"Hello ${user.name}"))
-
-  val booksListingServerEndpoint: ServerEndpoint[Any, IO] = booksListing.serverLogicSuccess(_ => IO.pure(Library.books))
-
-  val apiEndpoints: List[ServerEndpoint[Any, IO]] = List(helloServerEndpoint, booksListingServerEndpoint) ++ endpoints
+object Endpoints extends FilesServerEndpoints {
+  val apiEndpoints: List[ServerEndpoint[Any, IO]] = endpoints
 
   val docEndpoints: List[ServerEndpoint[Any, IO]] = SwaggerInterpreter()
     .fromServerEndpoints[IO](apiEndpoints, "local-share", "1.0.0")
